4806052f508fac8563f0a5ebefb17afd1e74b8e5
megahomyak <g.megahomyak@gmail.com>
2024-11-29 00:39:57 +0500

    Having some good ideas about how to not repeat code in the simple, "brute force" parser

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
-enum Command {
-    LoadName(String),
-    Invocation(Program),
+enum Part {
+    Text(String),
+    Group(Group),
 }
 
 type Index = usize;
-struct Program {
-    commands: Vec<(Command, Index)>,
+struct Group {
+    commands: Vec<(Part, Index)>,
 }
 
-struct Iter<'a> {
+struct Parser<'a> {
     idx: usize,
     s: &'a str,
+    unexpected_closers: Vec<Index>,
+    unclosed_openers: Vec<Index>,
 }
-impl<'a> Iter<'a> {
+
+impl<'a> Parser<'a> {
     fn next(&mut self) -> Option<char> {
         let c = unsafe { &self.s.get_unchecked(self.idx..) }.chars().next();
         if let Some(c) = c {
@@ -20,10 +23,35 @@ impl<'a> Iter<'a> {
         }
         c
     }
-}
-
-fn parse(i: &mut Iter) -> Program {
-
+    fn parse_list(&mut self) -> Group {
+        let mut root = Group { commands: vec![] };
+        let mut str_idx = self.idx;
+        let mut current_level = &mut root;
+        let mut levels = Vec::new();
+        let mut strbuf = String::new();
+        loop {
+            match self.next() {
+                c @ (None | Some(')') | Some('(')) => {
+                    if !strbuf.is_empty() {
+                        current_level.commands.push((Part::Text(strbuf), str_idx));
+                        strbuf = String::new();
+                        str_idx = self.idx;
+                    }
+                    match c {
+                        None => break,
+                        Some('(') => levels.push(),
+                        Some(')') => ,
+                    }
+                },
+                Some(c) => {
+                    strbuf.push(c);
+                },
+            }
+        }
+    }
+    fn parse(&mut self) -> Group {
+    
+    }
 }
 
 fn main() {}
