58e06743b9225155a7bb4cd6a2055b80d60d6828
megahomyak <g.megahomyak@gmail.com>
2024-11-30 22:12:22 +0500

    Added escaping

--- src/draft.py
+++ src/draft.py
@@ -1,7 +1,7 @@
 root = []
 stack = []
 
-input_ = "abc(def(g()h))i)blah(("
+input_ = r"abc(d\(ef(\\g()h))i)blah((" + "\\"
 idx = 0
 
 sbuf = ""
@@ -9,6 +9,8 @@ sbufidx = 0
 
 unclosed_openers = []
 unexpected_closers = []
+
+escaped = False
 
 def nextiter(): # Just some Python convenience
     global idx
@@ -23,6 +25,10 @@ def nextiter(): # Just some Python convenience
 while True:
     curidx = idx
     c = nextiter()
+    if escaped and c is not None:
+        sbuf += c
+        escaped = False
+        continue
     if c == "(" or c == ")" or c is None:
         try:
             top = stack[-1][1] # [1] = "take the actual group, not the idx" (which is also in the same tuple)
@@ -45,7 +51,10 @@ while True:
             except IndexError:
                 unexpected_closers.append(curidx)
     else:
-        sbuf += c
+        if c == "\\":
+            escaped = True
+        else:
+            sbuf += c
 for groupidx, _group in stack:
     unclosed_openers.append(groupidx)
     # Dealloc memory for (curidx, new) here
