ca38fb2c32c38a17ebade3dec8c08be49bb21711
megahomyak <g.megahomyak@gmail.com>
2024-12-07 15:52:37 +0500

    In progress...

--- c/main.c
+++ c/main.c
@@ -1,13 +1,6 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-struct ParsingResult {
-    void* unclosed_openers;
-    void* unexpected_closers;
-    size_t root_length;
-    void* root;
-};
-
 enum Result {
     SUCCESS,
     FAILURE,
@@ -34,102 +27,70 @@ OUTPUT:
 */
 enum Result parse(struct ParsingResult* parsing_result, char* input) {
     /* Stage 1 */
-    size_t unclosed_openers_count = 0;
-    size_t max_overlay_level = unclosed_openers_count;
-    size_t idx = 0;
-    size_t unexpected_closers_count = 0;
-    size_t nodes_amount = 0;
-    size_t sbufsize = 0;
-    bool escaped = false;
-    char c;
-    for (;;) {
-        c = input[idx];
-        if (escaped && c != '\0') {
-            escaped = false;
-            if (!(c == '(' || c == ')' || c == '\\')) {
-                ++sbufsize;
-            }
-            ++sbufsize;
-            continue;
-        }
-        if (c == '\0' || c == '(' || c == ')') {
-            if (sbufsize != 0) {
-                ++nodes_amount;
-                sbufsize = 0;
-            }
+    size_t overlays_amount = 0;
+    size_t maxstack = 0;
+    {
+        size_t idx = 0;
+        bool escaped = false;
+        size_t curstack = 0;
+        for (;;) {
+            char c = input[idx++];
             if (c == '\0') break;
+            if (escaped) {
+                escaped = false;
+                continue;
+            }
             if (c == '(') {
-                ++nodes_amount;
-                ++unclosed_openers_count;
-                if (unclosed_openers_count > max_overlay_level) {
-                    max_overlay_level = unclosed_openers_count;
-                }
-            } else {
-                if (unclosed_openers_count == 0) {
-                    ++unexpected_closers_count;
-                } else {
-                    --unclosed_openers_count;
-                }
-            }
-            continue;
-        } else if (c == '\\') escaped = true;
-        else ++sbufsize;
-        ++idx;
-    }
-    /* Stage 2 */
-    size_t overlays_size = max_overlay_level * sizeof(size_t*) + nodes_amount * sizeof(size_t);
-    void* overlays;
-    void* node_lengths;
-    if (overlays_size != 0) {
-        overlays = malloc(overlays_size);
-        if (overlays == NULL) return FAILURE;
-        node_lengths = overlays + max_overlay_level * sizeof(size_t*);
-    }
-    size_t root_length = 0;
-    idx = 0;
-    escaped = false;
-    unclosed_openers_count = 0;
-    for (;;) {
-        c = input[idx];
-        if (escaped && c != '\0') {
-            escaped = false;
-            if (!(c == '(' || c == ')' || c == '\\')) {
-                ++sbufsize;
-            }
-            ++sbufsize;
-            continue;
+                ++overlays_amount;
+                ++curstack;
+                if (curstack > maxstack) maxstack = curstack;
+            } else if (c == ')') {
+                if (curstack != 0) --curstack;
+            } else if (c == '\\') escaped = true;
         }
-        if (c == '\0' || c == '(' || c == ')') {
-            if (sbufsize != 0) {
-                ++nodes_amount;
-                sbufsize = 0;
-            }
-            if (c == '\0') break;
-            if (c == '(') {
-                ++nodes_amount;
-                ++unclosed_openers_count;
-                if (unclosed_openers_count > max_overlay_level) {
-                    max_overlay_level = unclosed_openers_count;
-                }
-            } else {
-                if (unclosed_openers_count == 0) {
-                    ++unexpected_closers_count;
-                } else {
-                    --unclosed_openers_count;
-                }
-            }
-            continue;
-        } else if (c == '\\') escaped = true;
-        else ++sbufsize;
-        ++idx;
     }
-    if (overlays_size != 0) {
-        free(overlays);
-    }
-    /* Stage 3 */
-    return SUCCESS;
-}
 
-void free_parsing_result(struct ParsingResult* r) {
-    free(r->unclosed_openers);
+    /* Stage 2 */
+    size_t root_length = 0;
+    size_t* overlay_lengths;
+    {
+        size_t** stack;
+        if (maxstack != 0) {
+            overlay_lengths = malloc(overlays_amount * sizeof(size_t) + maxstack * sizeof(size_t*));
+            if (overlay_lengths == NULL) return FAILURE;
+            stack = (size_t**) &overlay_lengths[overlays_amount];
+        }
+        size_t idx = 0;
+        size_t escaped = false;
+        size_t stacklen = 0;
+        bool currently_on_text = false;
+        for (;;) {
+            char c = input[idx++];
+            if (c != '\0' && escaped) {
+                currently_on_text = true;
+                escaped = false;
+                continue;
+            }
+            if (c == '\0' || c == '(' || c == ')') {
+                if (currently_on_text) {
+                    currently_on_text = false;
+                    if (stacklen == 0) ++root_length;
+                    else (*stack[stacklen - 1])++;
+                }
+                if (c == '\0') break;
+                if (c == '(') {
+                    stack[stacklen++] = ;
+                } else if (c == ')') {
+                    if (stacklen != 0) --stacklen;
+                }
+            } else if (c == '\\') {
+                escaped = true;
+            } else {
+                currently_on_text = true;
+            }
+        }
+    }
+
+    /* Stage 3 */
+    if (maxstack != 0) free(overlay_lengths);
 }
